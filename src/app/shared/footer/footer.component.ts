de5f1a6474ef0b8f99a33c8988828206660bd835
Igor <igor.borowski@capgemini.com>
2022-09-09 18:16:49 +0200

    add shared to root module

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'Footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
