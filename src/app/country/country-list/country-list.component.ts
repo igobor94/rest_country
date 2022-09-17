e1124671f1a9bc1e90bc79e6ce94061b55bc68e2
Igor <igor.borowski@capgemini.com>
2022-09-17 17:44:25 +0200

    change tag selector to attribute

--- src/app/country/country-list/country-list.component.ts
+++ src/app/country/country-list/country-list.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-country-list',
+  selector: '[country-list]',
   templateUrl: './country-list.component.html',
   styleUrls: ['./country-list.component.scss']
 })
 export class CountryListComponent implements OnInit {
+  
+  @Input() countries: Array<Object> = []
 
   constructor() { }
 
