11752fa09a708b3cbdbee7859a971e5d2003a441
Igor <igor.borowski@capgemini.com>
2022-09-18 20:41:47 +0200

    create searchCountry service

--- src/app/core/services/country.service.ts
+++ src/app/core/services/country.service.ts
@@ -1,19 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, filter } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-
   url: string = 'https://restcountries.com/v3.1/all'
 
   constructor(private http: HttpClient) { }
 
-  getCountries() {
-    return this.http.get<Array<Object>>(this.url)
+  getCountries(): Observable<Object[]> {
+    return this.http.get<Array<Object>>(this.url).pipe(map((response: Object[]) => response))
+  }
+  
+
+  testLog(): void {
+    // console.log(this.searchCountry({searchInput: 'Poland'}).subscribe(val => console.log(val)))
   }
 
-  
+  searchCountry(_country: any) {
+    return this.http.get<Array<Object>>(this.url).pipe(map(country => country.filter((country: any) => country.name.common.toLowerCase().includes(_country.value.searchInput.toLowerCase()))))
+  }
+
+  filterCountrySubject() {
+
+  }
+
+  // filterCountries() {
+  //   return this.http.get<Array<Object>>(this.url).pipe(map())
+  // }
+
+
 }
